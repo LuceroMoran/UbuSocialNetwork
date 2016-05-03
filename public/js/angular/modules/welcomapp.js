09e657ec6b23d2d511edf440307e872bb1bdc68f
Edgar Arroyo <hi.ed@hotmail.com>
2016-05-03 14:15:11 -0500

    hotfix

--- public/js/angular/modules/welcomapp.js
+++ public/js/angular/modules/welcomapp.js
@@ -1,5 +1,10 @@
 var video = document.getElementById('bgvid');
 video.play();
+document.getElementsByTagName('video')[0].onended = function () {
+  this.load();
+  this.play();
+};
+
 var welcomeapp = angular.module('welcomeapp',['ui.router']);
 welcomeapp.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/landing');
